01a5eb5018aadbeef996d870f6843230fdd0f2b9
Rayane Porto de Jesus Fonseca <rayane15526@gmail.com>
2025-07-29 16:19:02 -0300

    Delete ex05.py

--- ex05.py
+++
@@ -1,6 +0,0 @@
-salario = float (input("Digite quanto você ganha por hora: "))
-hora = float (input("Digite a quantidade de horas trabalhadas no mês: "))
-
-multiplicacao = salario * hora
-
-print (f"Salário Mensal: {multiplicacao}")
