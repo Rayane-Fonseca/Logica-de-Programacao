68941fb5ac7a9ad77de39de150e0d3e602879993
Rayane Porto de Jesus Fonseca <rayane15526@gmail.com>
2025-07-29 16:18:34 -0300

    Delete ex03.py

--- ex03.py
+++
@@ -1,9 +0,0 @@
-notaum = int (input("Digite a nota do 1° Bimestre: "))
-notadois = int (input("Digite a nota do 2° Bimestre: "))
-notatres = int (input("Digite a nota do 3° Bimestre: "))
-notaquatro = int (input("Digite a nota do 4° Bimestre: "))
-
-soma = (notaum + notadois + notatres + notaquatro)/4
-
-print (f"Sua média é: {soma}")
-
