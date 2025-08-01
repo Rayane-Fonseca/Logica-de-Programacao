ab93394bee929177bc2e082a32258e70c463fcbd
Rayane Porto de Jesus Fonseca <rayane15526@gmail.com>
2025-08-01 16:46:59 -0300

    Add files via upload

---
+++ dicionarios.py
@@ -0,0 +1,9 @@
+# Dicionário: {chave:valor}
+
+dicionario = {"Primeiro":1, "Segundo":2, "Terceiro":3}
+
+dicionario1 = dict(primero=1, segundo=2, terceiro=3)
+dicionario2 = dict([("Primeiro",1),("Segundo",2),("Terceiro",3)])
+
+print(dicionario1)
+print(dicionario2)
---
+++ tupla.py
@@ -0,0 +1,4 @@
+# Acessar elemento
+
+minha_tupla = (10, 20, 30)
+print(minha_tupla[0])
